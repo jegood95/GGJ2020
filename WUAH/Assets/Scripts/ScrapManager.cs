9e37feb55c7579a26feda05bf4c4aae8ebac3f81
Joshua Good <jegood95@gmail.com>
2020-02-01 14:46:44 -0500

    Reworked UI to dynamically load Scrap data from ScrapManager.

---
+++ WUAH/Assets/Scripts/ScrapManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrapManager : MonoBehaviour
+{
+    public List<ScrapData> StartingScraps = new List<ScrapData>();
+    public GameObject ScrapInventoryUI;
+
+    void Awake()
+    {
+        ScrapUIManager scrapUIMgr = ScrapInventoryUI.GetComponent<ScrapUIManager>();
+
+        if (ScrapInventoryUI != null)
+        {
+            // Initialize scrap UI
+            foreach (ScrapData scrap in StartingScraps)
+            {
+                scrapUIMgr.InitializeScrapUI(scrap.Texture);
+            }
+        }
+        else
+        {
+            Debug.Log("Couldn't find ScrapUIManager in scene");
+        }
+    }
+
+    // TODO: collection logic
+}
---
+++ WUAH/Assets/Scripts/ScrapUIManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrapUIManager : MonoBehaviour
+{
+    public GameObject DisplayContents;
+    public GameObject ScrapUIPrefab;
+    
+    public void InitializeScrapUI(Texture inScrapTexture)
+    {
+        GameObject scrapUI = Instantiate(ScrapUIPrefab);
+        RawImage scrapImage = scrapUI.GetComponent<RawImage>();
+        scrapImage.texture = inScrapTexture;
+
+        scrapUI.transform.SetParent(DisplayContents.transform, worldPositionStays: false);
+    }
+}
